9c5572c51a65e01be028467fa9c5e7a8e0870c4a
Ivan Salipurevic <ivansalipurevic@192.168.1.2>
2025-06-16 13:41:46 +0200

    Dodao ResultsScreen i QuestionsSummary, izmijenjeni postojeći fajlovi

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -3,7 +3,9 @@ import 'package:adv_basics/answer_button.dart';
 import 'package:adv_basics/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
-  const QuestionsScreen({super.key});
+  const QuestionsScreen({super.key, required this.onSelectAnswer});
+
+  final void Function(String answer)onSelectAnswer;
 
   @override
   State<QuestionsScreen> createState() {
@@ -12,25 +14,51 @@ class QuestionsScreen extends StatefulWidget {
   }
 }
 class _QuestionsScreenState extends State<QuestionsScreen>{
+  var currentQuestionIndex = 0;
+
+  void answerQuestion(String selectedAnswer){
+    widget.onSelectAnswer(selectedAnswer);
+    //currentQuestionIndex = currentQuestionIndex + 1;
+    //currentQuestionIndex += 1;
+    setState((){
+      currentQuestionIndex++;
+    
+    });
+
+    
+  }
+
+
+
   @override
   Widget build (context){
-    final currentQuestion = questions[0];
+    final currentQuestion = questions[currentQuestionIndex];
 
     return SizedBox(
       width:double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children:  [
-        Text(
-          currentQuestion.text,
-          style: TextStyle(
-            color:Colors.white,
-        )),
-        const SizedBox(height: 30),
-        ...currentQuestion.answers.map((answer) {
-          return AnswerButton(answerText:answer, onTap: () {});
-        }),
-      ],),
+      child: Container(
+        margin:EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children:  [
+          Text(
+            currentQuestion.text,
+            style: TextStyle(
+              color:Colors.white,
+          ),
+            textAlign: TextAlign.center
+          ),
+          const SizedBox(height: 30),
+          ...currentQuestion.getShuffledAnswers().map((answer) {
+            return AnswerButton(answerText:answer, onTap:(){
+              answerQuestion(answer);
+            }
+            
+            );
+          }),
+        ],),
+      ),
     );
   }
 
